16292c10266f56e62704932817854c2a582a7a53
jeongseoe <gldi1633@naver.com>
2023-08-07 19:29:22 +0900

    feat : 공부하고서 재풀이 성공

--- programmers/기초트레이닝/8월/5일/03_문자열-섞기.js
+++ programmers/기초트레이닝/8월/5일/03_문자열-섞기.js
@@ -4,9 +4,6 @@
 // 두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서
 // 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.
 
-// slice 공부
-//
-
 function solution(str1, str2) {
-  for (let i = 0; i < str1.length; i++) {}
+  return [...str1].map((a, i) => a + str2[i]).join('');
 }
