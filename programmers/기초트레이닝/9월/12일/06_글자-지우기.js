b0076188c520783fd44055bee9af3ae234356419
jeongseoe <gldi1633@naver.com>
2023-10-04 04:56:49 +0900

    fix : 추가적인 공부 및 풀이

--- programmers/기초트레이닝/9월/12일/06_글자-지우기.js
+++ programmers/기초트레이닝/9월/12일/06_글자-지우기.js
@@ -9,6 +9,18 @@ function solution(my_string, indices) {
 }
 
 console.log(solution('apporoograpemmemprs', [1, 16, 6, 15, 0, 10, 11, 3]));
+
 // indices 를 반복문으로 돌려서 문자열로 넣으면어떨지?
 // join메서드를 따로 쓰지않아도된다.
-//
+function solution1(my_string, indices) {
+  let answer = '';
+
+  for (let i = 0; i < my_string.length; i++) {
+    if (!indices.includes(i)) {
+      answer += my_string[i];
+    }
+  }
+  return answer;
+}
+
+console.log(solution1('apporoograpemmemprs', [1, 16, 6, 15, 0, 10, 11, 3]));
