92ca80189dcee560d8f6adf6ee4e4fa4037bcc3b
jeongseoe <gldi1633@naver.com>
2023-11-12 00:54:04 +0900

    feat : 11/11 기초테스트 부분 조금 고민했던것만 올리기

---
+++ programmers/입문/11월/11일/03_최댓값만들기.js
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120847
+
+// 정수 배열 numbers가 매개변수로 주어집니다.
+// numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+  let 내림차순 = numbers.sort((a, b) => b - a);
+
+  const [a, b] = 내림차순;
+  return a * b;
+}
+
+console.log(solution([1, 2, 3, 4, 5]));
